83ef861969f059fea9bdeed24cc45f66b3e9b3e0
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-03-15 22:56:27 +0300

    Add easy hw_5

---
+++ hw_5/easy.py
@@ -0,0 +1,22 @@
+import asyncio
+import time
+
+import aiofiles
+import aiohttp
+
+URL = 'https://thisartworkdoesnotexist.com'
+N = 20
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        for i in range(N):
+            async with session.get(URL) as response:
+                async with aiofiles.open(f"artifacts/easy/image_{i + 1}.png", "bw") as file:
+                    await file.write((await response.content.read()))
+            if i != N - 1:
+                time.sleep(1)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
